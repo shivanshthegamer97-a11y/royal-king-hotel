2d3447e4462598310e136308f30b980b18115418
shivanshthegamer97-a11y <shivanshthegamer97@gmail.com>
2025-12-26 12:20:07 +0530

    Normal update

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let isDay = true;
 
 function toggleExterior(){
   const hero = document.querySelector('.hero');
+
   if(isDay){
     hero.style.backgroundImage =
-      "linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.6)), url('https://drive.google.com/uc?id=153O8rmBTlUlRD_r3_zLFiHWwoIunOa4V')";
+      "linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.6)), url('assets/exteriornight.jpg')";
   }else{
     hero.style.backgroundImage =
-      "linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.6)), url('https://drive.google.com/uc?id=1HvHIIZNQFVLH2GO44ukp5uofyimk6ccN')";
+      "linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.6)), url('assets/exteriorday.jpg')";
   }
+
   isDay = !isDay;
 }
